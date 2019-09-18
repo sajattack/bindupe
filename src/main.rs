b101b7d973b7b27ddf56bf93dbbc3f18f0a6f9d1
Paul Sajna <sajattack@gmail.com>
2019-09-17 19:47:06 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::io::BufWriter;
+use std::io::Error;
+use std::io::copy;
+use std::io::SeekFrom;
+use clap::{Arg, App};
+
+fn main() -> Result <(), Error>{
+    let matches = App::new("bindupe")
+        .version("0.1")
+        .author("Paul Sajna <sajattack@gmail.com>")
+        .about("Duplicates bytes of infile ndupes times in outfile")
+        .arg(Arg::with_name("infile")
+            .help("Sets the input file to use")
+            .required(true)
+            .index(1))
+        .arg(Arg::with_name("outfile")
+            .help("Sets the output file to use")
+            .required(true)
+            .index(2))
+        .arg(Arg::with_name("ndupes")
+            .help("Sets the number of times to duplicate bytes of infile to outfile")
+            .required(true)
+            .index(3))
+        .get_matches();
+
+    let mut infile = File::open(matches.value_of("infile").unwrap())?;
+    let mut outfile = File::create(matches.value_of("outfile").unwrap())?;
+    let mut reader = BufReader::new(infile);
+    let mut writer = BufWriter::new(outfile);
+    let ndupes = usize::from_str_radix(matches.value_of("ndupes").unwrap(), 10).unwrap();
+    for _ in 0..ndupes { 
+        copy(&mut reader, &mut writer)?;
+        reader.seek(SeekFrom::Start(0));
+    }
+    Ok(())
+}
